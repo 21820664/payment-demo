cc73311ce5052e203f2e35e6a123913c572b29a0
WU <21820664@qq.com>
2022-08-23 13:50:40 +0800

    2.0 引入Swagger

--- src/main/java/com/hsxy/paymentdemo/controller/ProductController.java
+++ src/main/java/com/hsxy/paymentdemo/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.hsxy.paymentdemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author WU
  * @Date 2022/8/23 11:29
  */
+@Api(tags = "商品管理 Product")
 @RestController
 @RequestMapping("/api/product")
 @CrossOrigin //跨域
 public class ProductController {
+	
+	@ApiOperation("Test接口")
 	@GetMapping("/test")
 	public String test(){
 		return "hello";
