1cfc265cef1b86dc25b572036ea2e0b98b3f8fbf
WU <21820664@qq.com>
2022-08-23 15:04:57 +0800

    3 定义统一结果AjaxResult

--- src/main/java/com/hsxy/paymentdemo/controller/ProductController.java
+++ src/main/java/com/hsxy/paymentdemo/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.hsxy.paymentdemo.controller;
 
+import com.hsxy.paymentdemo.vo.AjaxResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @name ProductController
@@ -16,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "商品管理 Product")
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin //跨域
+@CrossOrigin //跨域P
 public class ProductController {
 	
 	@ApiOperation("Test接口")
 	@GetMapping("/test")
-	public String test(){
-		return "hello";
+	public AjaxResult test(){
+		return AjaxResult.ok()
+				.data("msg","hello")
+				.data("now",new Date());
 	}
 }
---
+++ src/main/java/com/hsxy/paymentdemo/vo/AjaxResult.java
@@ -0,0 +1,56 @@
+package com.hsxy.paymentdemo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @name AjaxResult
+ * @Description JSON统一结果返回:操作消息提醒
+ * @author WU
+ * @Date 2022/8/23 13:59
+ */
+@Data
+@AllArgsConstructor
+public class AjaxResult {
+	private Integer code;
+	private String message;
+	//不用private Object data;∵可能带多个对象?
+	private Map<String, Object> data = new HashMap<>();
+	
+	public AjaxResult(int code, String msg)
+	{
+		this.code = code;
+		this.message = msg;
+		/*this.data("code", code);
+		this.data("message", msg);*/
+	}
+	public static AjaxResult ok(){
+		/*AjaxResult r = new AjaxResult();
+		r.setCode(0);
+		r.setMessage("成功");
+		return r;*/
+		return new AjaxResult(200,"成功");
+	}
+	
+	public static AjaxResult error(){
+		/*AjaxResult r = new AjaxResult();
+		r.setCode(-1);
+		r.setMessage("失败");
+		return r;*/
+		return new AjaxResult(-1,"失败");
+	}
+	
+	/**
+	 * @Description 方便链式调用
+	 * @Param [key, value] {键,值}
+	 * @return com.hsxy.paymentdemo.vo.AjaxResult {数据对象}
+	 */
+	public AjaxResult data(String key, Object value){
+		//super.put(key, value);
+		this.data.put(key, value);
+		return this;
+	}
+}
